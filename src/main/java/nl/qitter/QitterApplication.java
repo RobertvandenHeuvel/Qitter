db1c970f13db6a4482af1d913b56f08a0d9dbb9b
Robert van den Heuvel <robert_vd_heuvel@hotmail.com>
2019-05-31 10:15:52 +0200

    eerste commit

---
+++ src/main/java/nl/qitter/QitterApplication.java
@@ -0,0 +1,13 @@
+package nl.qitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QitterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/qitter/QitterApplicationTests.java
@@ -0,0 +1,16 @@
+package nl.qitter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QitterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
