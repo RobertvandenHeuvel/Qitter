efa3a842e3c89f111a896667c8757ade73e8df76
Robert van den Heuvel <robert_vd_heuvel@hotmail.com>
2019-05-31 13:21:18 +0200

    Robert: Emiel update ophalen

--- src/main/java/nl/qitter/api/GebruikerApi.java
+++ src/main/java/nl/qitter/api/GebruikerApi.java
@@ -1,7 +1,13 @@
 package nl.qitter.api;
 
+import java.util.Optional;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -14,14 +20,35 @@ import nl.qitter.services.GebruikerService;
 
 @Component
 @Path("gebruiker")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class GebruikerApi {
 	@Autowired
 	GebruikerService gebruikerService;
-	
+
+	@POST
+	public Response postGebruiker(Gebruiker gebruiker) {
+		Gebruiker gebruikerPost = gebruikerService.saveGebruiker(gebruiker);
+		return Response.ok(gebruikerPost).build();
+	}
+
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
 	public Response getAlleGebruikers() {
 		Iterable<Gebruiker> deGebruikers = gebruikerService.findAlleGebruikers();
 		return Response.ok(deGebruikers).build();
+	}
+
+	@GET
+	@Path("{id}")
+	public Response getGebruikerById(@PathParam("id") long id) {
+		Optional<Gebruiker> gebruiker = gebruikerService.findGebruikerById(id);
+		return Response.ok(gebruiker.get()).build();
+	}
+
+	@DELETE
+	@Path("{id}")
+	public Response deleteGebruiker(@PathParam("id") long id) {
+		gebruikerService.deleteGebruikerById(id);
+		return Response.status(Response.Status.OK).build();
 	}
 }
